d9eae60d6a4e360d005eae3bf68b0ee99bfa1531
inngutorova <inngutorova@gmail.com>
2022-10-02 19:07:29 +0300

    Реализована функции encrypt_caesar() и decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,39 +1,26 @@
 import typing as tp
 
-
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in plaintext:
+        if (65 <= ord(i) <= 90-shift) or (97 <= ord(i) <= 122-shift):
+            ciphertext += chr(ord(i) + shift)
+        elif (90-shift+1 <= ord(i) <= 90) or (122-shift+1 <= ord(i) <= 122):
+            ciphertext += chr(ord(i) - 26 + shift)
+        else:
+            ciphertext += i
     return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in ciphertext:
+        if (65+shift <= ord(i) <= 90) or (97+shift <= ord(i) <= 122):
+            plaintext += chr(ord(i) - shift)
+        elif (65 <= ord(i) <= 65+shift-1) or (97 <= ord(i) <= 97+shift-1):
+            plaintext += chr(ord(i) + 26 - shift)
+        else:
+            plaintext += i
     return plaintext
 
 
@@ -44,3 +31,4 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     best_shift = 0
     # PUT YOUR CODE HERE
     return best_shift
+
